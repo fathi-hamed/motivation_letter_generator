9139283f2df47387ae0edbd8e940ba6d70ea3372
fathi-hamed <fathi.hamed@supcom.tn>
2024-10-12 14:56:20 +0100

    'full_project'

---
+++ app/main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+
+from chains import Chain
+from portfolio import Portfolio
+from utils import clean_text
+
+
+def create_streamlit_app(llm, portfolio, clean_text):
+    st.title("📧 Motivation letter generator")
+    url_input = st.text_input("Enter a URL:", value="enter a url")
+    submit_button = st.button("Submit")
+
+    if submit_button:
+        try:
+            loader = WebBaseLoader([url_input])
+            data = clean_text(loader.load().pop().page_content)
+            portfolio.load_portfolio()
+            jobs = llm.extract_jobs(data)
+            for job in jobs:
+                skills = job.get('skills', [])
+                motivation=llm.write_motivation_letter(job)
+                st.code(motivation, language='markdown')
+        except Exception as e:
+            st.error(f"An Error Occurred: {e}")
+
+
+if __name__ == "__main__":
+    chain = Chain()
+    portfolio = Portfolio()
+    st.set_page_config(layout="wide", page_title="Motivation letter", page_icon="📧")
+    create_streamlit_app(chain, portfolio, clean_text)
+
+
